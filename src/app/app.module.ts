43e19691452583fb63e97fc449abde2ae5c7ee38
Sushovan <sushovan.nits@gmail.com>
2017-10-08 20:37:54 +1100

    Week1 exercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import {MaterialModule} from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import 'hammerjs';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +13,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
