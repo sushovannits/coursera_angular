72ba16b94af84474d80ebda6a340a3f034c11edd
Sushovan <sushovan.nits@gmail.com>
2017-10-09 22:47:31 +1100

    Week1 Last Part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
-import {MaterialModule} from './material.module';
+import {MatToolbarModule, MatButtonModule, MatCheckboxModule, MatListModule, MatGridListModule, MatCardModule} from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import 'hammerjs';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule,
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatListModule, 
+    MatGridListModule, 
+    MatCardModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [],  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
